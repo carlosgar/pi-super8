7875bde68e87d6e008a5116ff8d30e4683812350
vanessachammas <43588480+vanessachammas@users.noreply.github.com>
2021-05-24 22:25:42 -0700

    add record

--- record-video.py
+++ record-video.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+import io
 from picamera import PiCamera
 
-#test picture on raspi
+camera = PiCamera()
+
+stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.start_recording(stream, format='h264', quality=23)
+    camera.wait_recording(15)
+    camera.stop_recording()
