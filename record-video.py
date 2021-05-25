dd850d8fe1c7de00d1a4b1632a95fcd0cdb1c983
vanessachammas <43588480+vanessachammas@users.noreply.github.com>
2021-05-24 23:10:42 -0700

    added camera settings

--- record-video.py
+++ record-video.py
@@ -6,8 +6,41 @@ from picamera import PiCamera
 ##raspivid -o test1.h264 -t 20000 -ifx film -ISO 800 --shutter 28000 -awb auto -ex auto -fps 18 --mode 6
 stream = io.BytesIO()
 with PiCamera() as camera:
+
     camera.resolution = (640, 480)
     camera.framerate=18
+    camera.awb_mode = 'auto'
+        # 'off'
+        # 'auto'
+        # 'sunlight'
+        # 'cloudy'
+        # 'shade'
+        # 'tungsten'
+        # 'fluorescent'
+        # 'incandescent'
+        # 'flash'
+        # 'horizon'
+    #camera.brightness = 0 - 100
+    #color_effects = (128, 128)
+    camera.contrast = -20
+    camera.exposure_mode = 'auto'
+        # 'off'
+        # 'auto'
+        # 'night'
+        # 'nightpreview'
+        # 'backlight'
+        # 'spotlight'
+        # 'sports'
+        # 'snow'
+        # 'beach'
+        # 'verylong'
+        # 'fixedfps'
+        # 'antishake'
+        # 'fireworks'
+    camera.shutter_speed = 28000
+    camera.image_effect = 'film'
+    camera.image_effect_params.strength = 256.0
+    camera.iso = 200
     camera.start_preview()
     camera.start_recording(stream, format='h264', quality=20)
     camera.wait_recording(10)
