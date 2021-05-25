a5d713125c16ee9330413762adabb2e69b40f41d
vanessachammas <43588480+vanessachammas@users.noreply.github.com>
2021-05-24 22:29:34 -0700

    fix picamera import

--- record-video.py
+++ record-video.py
@@ -3,10 +3,8 @@
 import io
 from picamera import PiCamera
 
-camera = PiCamera()
-
 stream = io.BytesIO()
-with picamera.PiCamera() as camera:
+with PiCamera() as camera:
     camera.resolution = (640, 480)
     camera.start_recording(stream, format='h264', quality=23)
     camera.wait_recording(15)
